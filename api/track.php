c03903fe17fe026a72b4dc68b3e5f72ba17b8ef1
Antonio Melin <antoniomelin@gmail.com>
2025-01-11 14:13:18 -0300

    Update track.php

--- api/track.php
+++ api/track.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/config.php';
+$config = require __DIR__ . '/config.php';
 
 header('Content-Type: application/json');
 
@@ -32,11 +32,11 @@ $_SESSION['rate_limit'][$ip]['count']++;
 
 // Conexión a la base de datos
 $mysqli = new mysqli(
-    $config['host'],
-    $config['username'],
-    $config['password'],
-    $config['database'],
-    $config['port']
+  $config['host'],
+  $config['username'],
+  $config['password'],
+  $config['database'],
+  $config['port']
 );
 
 // Verifica conexión
