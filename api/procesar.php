3903e7b56f44ad2073f31f1f967c2bbfcbf3404e
Antonio Melin <antoniomelin@gmail.com>
2025-01-15 00:21:17 -0300

    Update procesar.php

--- api/procesar.php
+++ api/procesar.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             exit;
         }
 
-        $stmt = $mysqli->prepare("UPDATE interacciones SET estado = 'procesado' WHERE codigo_seguimiento = ?");
+        $stmt = $mysqli->prepare("UPDATE interacciones SET estado = 'en_procesado' WHERE codigo_seguimiento = ?");
         $stmt->bind_param('s', $id);
         $stmt->execute();
 
